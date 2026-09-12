8b844008deaddc8387bdf4566118ad9a7439346e
Kadek Julita <kadekbunga99@gmail.com>
2026-09-12 09:06:27 +0800

    Menambahkan route /admin

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('beranda');
 });
+
+
+Route::get('/admin', function () {
+    return view('backoffice.dashboard');
+});
