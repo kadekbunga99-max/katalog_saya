d6970c3b038b0807727b1743746633c755adb471
Kadek Julita <kadekbunga99@gmail.com>
2026-09-20 08:59:54 +0800

    Mengubah route /

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HalamanController;
 
-Route::get('/', function () {
-    return view('user_front.beranda');
-});
-
+Route::get('/', [HalamanController::class, 'home']);
 
 Route::get('/admin', function () {
     return view('backoffice.dashboard');
