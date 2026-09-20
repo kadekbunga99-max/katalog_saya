add0d081e482878446b1e0289a5a0b954c7277b9
Kadek Julita <kadekbunga99@gmail.com>
2026-09-20 10:58:34 +0800

    Update route

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HalamanController;
+use App\Http\Controllers\ProdukController;
 
-Route::get('/', [HalamanController::class, 'home']);
+Route::get('/', [HalamanController::class, 'home'])->name('home');
+Route::get('/kontak', [HalamanController::class, 'kontak'])->name('kontak');
+
+Route::get('/produk', [ProdukController::class, 'index'])->name('produk');
+Route::get('/produk/{id}', [ProdukController::class, 'show'])->name('produk.show');
 
 Route::get('/admin', function () {
     return view('backoffice.dashboard');
